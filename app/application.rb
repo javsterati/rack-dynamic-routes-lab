b6c1e61fa7e1a15886ed3dc032423e01a2dc2e8e
Javi Huerta <javsteratii@gmail.com>
2019-12-09 18:34:34 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,10 @@
 class App
-    
-    get 'items/name' do
 
+    get '/itmes/:id' do
+        @medicine = all_the_medicines.select do |medicine|
+            medicine.id == params[:id]
+        end.first
+        erb :'/medicines/show.html'
     end     
 
 end
